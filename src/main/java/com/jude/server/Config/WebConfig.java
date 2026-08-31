ea1e29793fd2eb9cad7a1b31978b7e4beb519e68
narag0123 <narag0123@gmail.com>
2026-08-31 14:54:37 +0900

    Fix Vercel CORS origin

--- src/main/java/com/jude/server/Config/WebConfig.java
+++ src/main/java/com/jude/server/Config/WebConfig.java
@@ -10,7 +10,10 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-            .allowedOrigins("http://localhost:3000", "http://localhost", "https://elec-maintenance-front.vercel.app/*")
+            .allowedOrigins(
+                "http://localhost:3000",
+                "http://localhost",
+                "https://elec-maintenance-front.vercel.app")
             .allowedMethods("*")
             .allowedHeaders("*")
             .allowCredentials(true);
